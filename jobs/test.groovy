9d246a229d99c77b4e2b101a798a8b7478e088d2
m.dynkin <m.dynkin@fbs.com>
2022-10-16 15:01:45 +0300

    2

--- jobs/test.groovy
+++ jobs/test.groovy
@@ -2,7 +2,7 @@
 
 // create an array with our two pipelines
 pipelines = ["test-python"]
-JENKINS_JOB_DSL_URL = "https://github.com/Dynkine/aurora.git"
+// JENKINS_JOB_DSL_URL = "https://github.com/Dynkine/aurora.git"
 // iterate through the array and call the create_pipeline method
 pipelines.each { pipeline ->
     println "Creating pipeline ${pipeline}"
@@ -18,7 +18,7 @@ def create_pipeline(String name) {
                 scm {
                     git {
                         remote {
-                          url "${JENKINS_JOB_DSL_URL}"
+                          url ("https://github.com/Dynkine/aurora.git")
                         {
                     {
                 }
