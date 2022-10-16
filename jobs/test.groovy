0a45a6e026cebce2c057dbe2aff7ab5077c5369c
m.dynkin <m.dynkin@fbs.com>
2022-10-16 18:17:18 +0300

    fix

--- jobs/test.groovy
+++ jobs/test.groovy
@@ -1,16 +1,5 @@
 #!groovy
 
-// // create an array with our two pipelines
-// pipelines = ["test-python"]
-// // JENKINS_JOB_DSL_URL = "https://github.com/Dynkine/aurora.git"
-// // iterate through the array and call the create_pipeline method
-// pipelines.each { pipeline ->
-//     println "Creating pipeline ${pipeline}"
-//     create_pipeline(pipeline)
-// }
-
-// // a method that creates a basic pipeline with the given parameter name
-// def create_pipeline(String name) {
 pipelineJob("python-job") {
     definition {
         cpsScm {
@@ -30,4 +19,3 @@ pipelineJob("python-job") {
         githubPush()
     }
 }
-// }
